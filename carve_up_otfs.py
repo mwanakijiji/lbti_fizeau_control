fdd6754b289b79d2a045a6dea272a1cb678b85f2
Eckhart <spalding@email.arizona.edu>
2020-06-30 13:30:22 -0700

    more info in plot titles

--- carve_up_otfs.py
+++ carve_up_otfs.py
@@ -37,6 +37,16 @@ def worker(file_name, q):
     img = f[0].data
     header = f[0].header # contains injected values
 
+    opd_inj_unwrapped = header["OPD_UM"]
+    inj_tip = header["TIPY_MAS"]
+    inj_tilt = header["TILTXMAS"]
+    if np.logical_and(header["X_SHIFT"],header["Y_SHIFT"]):
+        inj_delx = header["X_SHIFT"]
+        inj_dely = header["Y_SHIFT"]
+    else:
+        inj_delx = 0.
+        inj_dely = 0.
+
     # take x-sec in x
     x_sec = img[250,:]
 
@@ -44,6 +54,23 @@ def worker(file_name, q):
     y_sec_left = img[:,153]
     y_sec_middle = img[:,250]
     y_sec_right = img[:,348]
+
+    # calculate wrapped OPD
+    # injected quantities
+    '''
+    opd_inj_simple_mod = np.mod(opd_inj_unwrapped,wavel_um)
+    opd_inj_positive_side = np.copy(opd_inj_simple_mod)
+    opd_inj_negative_side = np.copy(opd_inj_simple_mod)
+
+    # injected OPDs, after wrapping, and within an OPD range of (0, +lambda/2)
+    opd_inj_positive_side[
+        ~np.logical_and(opd_inj_simple_mod >= 0,opd_inj_simple_mod < wavel_um/2)] = np.nan
+    # injected OPDs, after wrapping, and within an OPD range of (-lambda/2, 0)
+    opd_inj_negative_side[
+        ~np.logical_and(opd_inj_simple_mod >= wavel_um/2,opd_inj_simple_mod < wavel_um)] = np.nan
+    # translate the 'negative' side OPDs
+    opd_inj_negative_side -= wavel_um
+    '''
 
     # plot it: x-sec in x across the top, and 3 x-sec in y along the bottom
     fig=plt.figure()
@@ -62,7 +89,14 @@ def worker(file_name, q):
     ax2.set_title("x-sec in y, left")
     ax3.set_title("x-sec in y, middle")
     ax4.set_title("x-sec in y, right")
-    plt.suptitle(os.path.basename(file_name))
+    plt.suptitle(os.path.basename(file_name) + \
+        "\ninjected tip: " + str(inj_tip) + \
+        "\ninjected tilt: " + str(inj_tilt) + \
+        "\ninjected opd: " + str(opd_inj_unwrapped) + \
+        "\ninjected del_x: " + str(inj_delx) + \
+        "\ninjected del_y: " + str(inj_dely))
+
+    plt.tight_layout()
 
     fig.savefig(plot_write_name)
 
